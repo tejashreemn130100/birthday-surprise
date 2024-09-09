fd308ee476e2d2efc7fe4dcd7636b95663c3698d
tejashreemn130100 <tejashree130100@gmail.com>
2024-09-09 11:50:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const popup = document.getElementById("birthday-popup");
     const closeBtn = document.querySelector(".close");
     const surpriseBtn = document.getElementById("surprise-btn");
-    var popup = document.getElementById('popup');
-    var closeBtn = document.getElementById('closeBtn');
+
 
     // Show the popup when the page loads
     popup.style.display = "flex";
@@ -12,15 +11,8 @@ document.addEventListener("DOMContentLoaded", function() {
     closeBtn.onclick = function() {
         popup.style.display = "none";
     }
-function showPopup() {
-  popup.style.display = 'flex';
-}
     // Add a custom action when the button is clicked
     surpriseBtn.onclick = function() {
         alert("I LOVE U SIDDU ❤️ 🎉");
-        closeBtn.onclick = function() {
-  popup.style.display = 'none';
-}
-
     }
 });
